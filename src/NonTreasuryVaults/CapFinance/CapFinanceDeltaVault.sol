40a337d27e14ad52f845e10e2bc262fbcfa2c52e
Furyoku Syndicate <furyokusyndicate0x1@gmail.com>
2022-10-17 17:26:24 -0400

    change ERC4626 to solmate

--- src/NonTreasuryVaults/CapFinance/CapFinanceDeltaVault.sol
+++ src/NonTreasuryVaults/CapFinance/CapFinanceDeltaVault.sol
@@ -2,8 +2,9 @@
 
 pragma solidity 0.8.15;
 
-import {ERC4626, ERC20} from "openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import {IERC20} from "openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC4626} from "solmate/src/mixins/ERC4626.sol";
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {CapPositionHandler} from "./CapPositionHandler.sol";
 
 /**
  * Design Notes:
@@ -18,8 +19,12 @@ import {IERC20} from "openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CapFinanceDeltaVault is ERC4626 {
     constructor(
-        IERC20 _asset,
+        ERC20 _asset,
         string memory _name,
         string memory _symbol
-    ) ERC4626(_asset) ERC20(_name, _symbol) {}
+    ) ERC4626(_asset, _name, _symbol) {}
+
+    function totalAssets() public view virtual override returns (uint256) {
+        //pass
+    }
 }
